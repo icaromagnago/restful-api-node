3f71a138f2b81444aa7e214b944ea872f13772f2
Ícaro Magnago <icaro.magnago@anbima.com.br>
2021-04-19 20:11:07 -0300

    Node api

---
+++ index.js
@@ -0,0 +1,106 @@
+/*
+ Primary file for the API
+*/
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
+
+// Instantiate HTTP server
+const httpServer = http.createServer((req, res) => {
+    unifiedServer(req, res);
+});
+
+// Instantiate HTTPS server
+const httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+};
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+    unifiedServer(req, res);
+}); 
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, () => {
+    console.log(`The server is listening on port ${config.httpPort} in ${config.envName} mode`);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+    console.log(`The server is listening on port ${config.httpsPort} in ${config.envName} mode`);
+});
+
+const unifiedServer = (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path and removed / from the start and the end. /foo/bar/ => foo/bar
+    const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+
+    const queryStringObject = parsedUrl.query;
+    
+    const method = req.method.toLowerCase();
+
+    const headers = req.headers;
+
+    // Get the payload
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    //Streams
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Choose the handler this request should go to. If one is not found choose notfound
+        const chooseHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // Construct the data object to send to the handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        // Route the request to the handler
+        chooseHandler(data, (statusCode, payload) => {
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to empty
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            const payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
+    });
+}
+
+// Define the handlers
+const handlers = {};
+
+// Ping handler
+handlers.ping = (data, callback) => {
+    callback(200);
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+// Define a request router
+const router = {
+    'ping': handlers.ping
+}
