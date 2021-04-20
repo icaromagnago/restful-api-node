e807bc998c30ad72976bcd17dbf77e75d1a67cb4
Ícaro Magnago <icaro.magnago@anbima.com.br>
2021-04-20 09:05:34 -0300

    Users and tokens handlers

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
+const config = require('./lib/config');
 const fs = require('fs');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
 // Instantiate HTTP server
 const httpServer = http.createServer((req, res) => {
@@ -64,7 +66,7 @@ const unifiedServer = (req, res) => {
             'queryStringObject': queryStringObject,
             'method': method,
             'headers': headers,
-            'payload': buffer
+            'payload': helpers.parseJsonToObject(buffer)
         };
 
         // Route the request to the handler
@@ -87,20 +89,9 @@ const unifiedServer = (req, res) => {
     });
 }
 
-// Define the handlers
-const handlers = {};
-
-// Ping handler
-handlers.ping = (data, callback) => {
-    callback(200);
-};
-
-// Not found handler
-handlers.notFound = (data, callback) => {
-    callback(404);
-};
-
 // Define a request router
 const router = {
-    'ping': handlers.ping
+    'ping': handlers.ping,
+    'users': handlers.users,
+    'tokens': handlers.tokens
 }
